e79d24db3ca5d4cdf9b08966a5337d045772b26b
Sonal <117053184+one-shubham180@users.noreply.github.com>
2026-02-12 21:40:32 +0530

    Signed-off-by: Sonal <117053184+one-shubham180@users.noreply.github.com>

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -1,13 +1,40 @@
-﻿using API.DTOs;
+﻿using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interface;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace API.Controllers
 {
 
-	public class AuthController(ITokenService tokenService) : BaseApiController
+	public class AuthController(AppDBContext context,ITokenService tokenService) : BaseApiController
 	{
+		[HttpPost("regsiter")]
+		public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+		{
+			if (await context.Users.AnyAsync(x => x.Email == registerDto.Email)) return BadRequest("Email already exists");
+			var hmac = new HMACSHA512();
+			var user = new User
+			{
+				Email=registerDto.Email,
+				UserName=registerDto.UserName,
+				PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+				PasswordSalt = hmac.Key
+			};
+			await context.Users.AddAsync(user);
+			await context.SaveChangesAsync();
+			return new UserDto
+			{
+				Email = user.Email,
+				UserName = user.UserName,
+				Id = user.Id,
+				Token = tokenService.CreateToken(user)
+			};
+		}
+
 		[HttpPost("login")]
 		public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
 		{
---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DTOs
+{
+	public class RegisterDto
+	{
+		public required string UserName { get; set; }
+		public required string Email { get; set; }
+		public required string Password { get; set; }
+	}
+}
