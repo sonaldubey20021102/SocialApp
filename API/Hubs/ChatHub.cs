702cedcbc32646a4886cfbaa00c8b36ab05bf3d8
Sonal <117053184+one-shubham180@users.noreply.github.com>
2026-02-15 20:37:39 +0530

    Signed-off-by: Sonal <117053184+one-shubham180@users.noreply.github.com>

---
+++ API/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.Hubs
+{
+	public class ChatHub: Hub
+	{
+		public async Task SendMessage(User user, string message)
+		{
+
+		}
+	}
+}
